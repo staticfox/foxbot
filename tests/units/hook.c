5bcd671adad593dc53e0e526202a864d177c2f96
staticfox <staticfox@staticfox.net>
2016-05-21 02:39:15 -0400

    test: Add test for on_privmsg

--- tests/units/hook.c
+++ tests/units/hook.c
@@ -20,6 +20,7 @@
  *
  */
 
+#include <foxbot/channel.h>
 #include <foxbot/hook.h>
 #include <foxbot/list.h>
 
@@ -28,7 +29,7 @@
 
 static int passer = 0;
 
-void
+static void
 check_func(void)
 {
     passer++;
@@ -37,6 +38,7 @@ check_func(void)
 START_TEST(hook_check)
 {
     begin_test();
+    passer = 0;
     add_hook("check_hook", (hook_func) check_func);
     /* echo_test + check_hook */
     ck_assert_int_eq(hook_count(), 2);
@@ -49,6 +51,20 @@ START_TEST(hook_check)
 }
 END_TEST
 
+START_TEST(hook_privmsg)
+{
+    begin_test();
+    passer = 0;
+    add_hook("on_privmsg", (hook_func) check_func);
+    ck_assert_ptr_ne(find_channel("#unit_test"), NULL);
+    write_and_wait(":test_user1!~test@255.255.255.255 JOIN #unit_test");
+    write_and_wait(":test_user1!~test@255.255.255.255 PRIVMSG #unit_test :hi");
+    ck_assert_int_eq(passer, 1);
+    delete_hook("on_privmsg", (hook_func) check_func);
+    end_test();
+}
+END_TEST
+
 void
 hook_setup(Suite *s)
 {
@@ -56,6 +72,7 @@ hook_setup(Suite *s)
 
     tcase_add_checked_fixture(tc, NULL, delete_foxbot);
     tcase_add_test(tc, hook_check);
+    tcase_add_test(tc, hook_privmsg);
 
     suite_add_tcase(s, tc);
 }
